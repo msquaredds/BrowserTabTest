28b29e5cea340a2aaa008b57e1335a1a8a0f3396
msquaredds <alex.melesko@msquaredds.com>
2025-07-18 13:13:13 -0400

    Removing unnecessary sections

--- Test.py
+++ Test.py
@@ -2,8 +2,6 @@ import time
 
 import streamlit as st
 
-from streamlit_extras.stylable_container import stylable_container
-
 
 def _sleep_and_show_spinner():
     with st.spinner():
@@ -13,15 +11,6 @@ def _sleep_and_show_spinner():
 def main():
     st.title("Spinner Test Page")
 
-    # button_cols = st.sidebar.columns([1, 4, 2, 4, 1])
-    # with button_cols[1]:
-    # with stylable_container(
-    #         key="test_container",
-    #         css_styles="""
-    #             button {
-    #                 background-color: #1D867F;
-    #                 color: white;
-    #             }"""):
     st.button("Test Timer", on_click=_sleep_and_show_spinner)
 
 
--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 import streamlit as st
 
 
-
 def main():
     st.set_page_config(
         page_title="Test Title",
         layout="wide",
         page_icon="🕒")
-
-    # st.markdown(
-    #     """
-    #     <style>
-    #         section[data-testid="stSidebar"] {
-    #             width: 400px !important;
-    #         }
-    #     </style>
-    #     """,
-    #     unsafe_allow_html=True)
-
-    # col_title = st.columns(3)
-    # with col_title[1]:
-    #     st.image("Sharp_Shares_Title_Logo_Subtitle_06_27_2025.png")
 
     pages = [st.Page("Test.py", title="Test")]
     pg = st.navigation(pages)
